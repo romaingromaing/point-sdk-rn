4aab8595e81f0957e99e64200989c27fce43cf7f
Fabio Coelho <fccoelho7@gmail.com>
2022-04-08 08:40:04 -0300

    add types definition

---
+++ index.d.ts
@@ -0,0 +1,42 @@
+/// <reference types="typescript" />
+
+type Callback = (_: any, success: any) => void;
+type Permissions = ["example"];
+
+type User = {
+  id: string;
+  email: string;
+  firstName: string;
+  birthday: string;
+  goal: string;
+};
+
+type Workout = {
+  id: string;
+  calories: number;
+  distance: number;
+  start: Date;
+  end: Date;
+};
+
+export const healthPermissions: Permissions[];
+
+// Setup
+export function setup(
+  clientId: string,
+  clientSecret: string,
+  permissions: Permissions[],
+  callback: Callback
+): void;
+export function startBackgroundListener(): Promise<any>;
+export function stopBackgroundListener(): Promise<any>;
+export function setupBackgroundListener(): Promise<any>;
+export function requestPermissions(): Promise<any>;
+
+// Auth
+export function login(accessToken: string): Promise<any>;
+export function logout(): Promise<any>;
+
+// User Data
+export function getUserData(): Promise<User>;
+export function getUserWorkouts(): Promise<Workout>;
